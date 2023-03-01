17ecf2a6d3dd061411980b2d97a8bf433c929a3c
nicolas <nicolas@Air-de-nicolas.fibertel.com.ar>
2023-03-01 19:22:12 -0300

    actualizacion

--- src/components/Comida.js
+++ src/components/Comida.js
@@ -2,21 +2,27 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
 import { FontAwesome } from "react-native-vector-icons";
 
-
-
-export default function App() {
+function Comida() {
   const [cartItems, setCartItems] = useState([]);
   const [showCart, setShowCart] = useState(false);
   const productImage = require("../../comida.png");
   const products = [
-    { id: 1, name: "Barfer box perro pollo", price: 10 ,imageUri: productImage,},
-    { id: 2, name: "Barfer box perro pollo", price: 20 , imageUri: productImage, },
-    { id: 3, name: "Barfer box perro pollo", price: 30, imageUri: productImage, },
-    { id: 4, name: "Barfer box perro pollo", price: 40, imageUri: productImage, },
-    { id: 5, name: "Barfer box perro pollo", price: 50, imageUri: productImage, }
+    { id: 1, name: "Barfer box perro pollo", price: 1000, imageUri: productImage, },
+    { id: 2, name: "Barfer box perro pollo", price: 2340, imageUri: productImage, },
+    { id: 3, name: "Barfer box perro pollo", price: 3660, imageUri: productImage, },
+    { id: 4, name: "Barfer box perro pollo", price: 4230, imageUri: productImage, },
+    { id: 5, name: "Barfer box perro pollo", price: 5041, imageUri: productImage, }
   ];
 
- 
+  const productsCat = [
+    { id: 6, name: "Barfer box Gato pollo", price: 10, imageUri: productImage, },
+    { id: 7, name: "Barfer box perro pollo", price: 20, imageUri: productImage, },
+    { id: 8, name: "Barfer box perro pollo", price: 30, imageUri: productImage, },
+    { id: 9, name: "Barfer box perro pollo", price: 40, imageUri: productImage, },
+    { id: 10, name: "Barfer box perro pollo", price: 50, imageUri: productImage, }
+  ];
+
+
   const addToCart = (product) => {
     const existingItemIndex = cartItems.findIndex((item) => item.id === product.id);
     if (existingItemIndex !== -1) {
@@ -27,8 +33,6 @@ export default function App() {
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
   };
-    
-
 
   const removeItem = (itemId) => {
     setCartItems(cartItems.filter(item => item.id !== itemId));
@@ -37,16 +41,25 @@ export default function App() {
   const renderItem = ({ item }) => (
 
     <View style={styles.item_row}>
-    <View style={styles.item}>
-    <Image source={item.imageUri} style={styles.image} />
-      <Text style={styles.title}>{item.name}</Text>
-      <Text style={styles.title}>{item.price}</Text>
-      <TouchableOpacity style={styles.addButton} onPress={() => addToCart(item)}>
-        <Text style={styles.addButtonText}>Agregar</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.item}>
+        <Image source={item.imageUri} style={styles.image} />
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.price}>${item.price}</Text>
+
+        <View style={styles.contenedor_boton}>
+          <TouchableOpacity style={styles.addButton} onPress={() => addToCart(item)}>
+            <FontAwesome name="shopping-cart" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
+
+
+
+
+
+
 
   const cartTotal = cartItems.reduce((total, item) => total + item.price, 0);
 
@@ -60,7 +73,7 @@ export default function App() {
       <TouchableOpacity style={styles.cartButton} onPress={() => setShowCart(!showCart)}>
         <FontAwesome name="shopping-cart" size={30} color="black" />
         <Text style={styles.cartButtonText}> Total:{cartItems.length}</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
 
 
 
@@ -69,31 +82,43 @@ export default function App() {
       {showCart && (
         <View style={styles.cartContainer}>
           <TouchableOpacity onPress={() => setShowCart(false)}>
-              <FontAwesome name="close" size={30} color="black" />
-            </TouchableOpacity>
+            <FontAwesome name="close" size={30} color="black" />
+          </TouchableOpacity>
           <Text style={styles.cartTitle}>Carritos de compras</Text>
           <FlatList
             data={cartItems}
             renderItem={({ item }) => (
               <View style={styles.cartItem}>
-              <Image source={item.imageUri} style={styles.cartItemImage} />
-              <View style={styles.cartItemDetails}>
-                <Text>{item.name}</Text>
-                <Text>${item.price} x {item.quantity}</Text>
+                <Image source={item.imageUri} style={styles.cartItemImage} />
+                <View style={styles.cartItemDetails}>
+                  <Text>{item.name}</Text>
+                  <Text>${item.price} x {item.quantity}</Text>
+                </View>
+                <TouchableOpacity style={styles.removeButton} onPress={() => removeItem(item.id)}>
+                  <FontAwesome name="trash" size={18} color="black" />
+                </TouchableOpacity>
               </View>
-              <TouchableOpacity style={styles.removeButton} onPress={() => removeItem(item.id)}>
-                <FontAwesome name="trash" size={18} color="black" />
-              </TouchableOpacity>
-            </View>
             )}
             keyExtractor={item => item.id.toString()}
           />
         </View>
       )}
-      <FlatList
+
+      <Text style={styles.txt_dog}>Para perro</Text>
+      <FlatList style={styles.flatList_products_dog}
         data={products}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
+        horizontal
+      />
+
+
+      <Text style={styles.txt_cat}>Para gato</Text>
+      <FlatList style={styles.flatList_products_cat}
+        data={productsCat}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        horizontal
       />
     </View>
   );
@@ -106,12 +131,30 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 20,
   },
-  item_row:{
+  item_row: {
+    display: "flex",
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginVertical: 8,
     marginHorizontal: 16,
+  },
+  txt_dog: {
+    position: "relative",
+    bottom: 41,
+    fontWeight: "700",
+    fontSize: 15
+  },
+  txt_cat: {
+    fontWeight: "700",
+    fontSize: 15
+  },
+  flatList_products_dog: {
+    marginBottom: 60,
+    marginTop: -40
+  },
+  flatList_products_cat: {
+    marginBottom: 60
   },
   item: {
     width: 170,
@@ -137,38 +180,57 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   title: {
-    padding:5,
+    padding: 5,
     fontSize: 17,
-    textAlign:"center"
+    textAlign: "center",
+    color:"#171717",
+    fontWeight:"700"
+  },
+  price: {
+    padding: 5,
+    fontSize: 17,
+    textAlign: "center",
+    color:"#006AE3"
   },
   addButton: {
-    backgroundColor: '#3662FF',
+    display: "flex",
+    alignItems: "flex-end",
+    backgroundColor: '#D9D9D9',
+    borderRadius: "100%",
     padding: 10,
-    borderRadius: 5
+  },
+  contenedor_boton:{
+    display:"flex",
+    justifyContent:"center",
+    alignSelf:"flex-end",
+    padding:15
   },
   addButtonText: {
     color: 'white',
-    textAlign:"center"
+    textAlign: "center"
   },
   cartButton: {
-    display:"flex",
-    flexDirection:"row",
-    position:"relative",
-    bottom:240,
-    left:190
+    display: "flex",
+    flexDirection: "row",
+    position: "relative",
+    bottom: 240,
+    left: 190
 
   },
   cartButtonText: {
 
   },
-  cartTitle:{
-    fontSize:30,
-    color:"#0053B1",
-    fontWeight:"700"
+  cartTitle: {
+    fontSize: 30,
+    color: "#0053B1",
+    fontWeight: "700"
 
   },
   cartContainer: {
     backgroundColor: '#fff',
-  
+
   }
 });
+
+
+export default Comida
